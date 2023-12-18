5befb6dc093921b2f2a871d43a31fc70aced7a1a
Dipayan Das <dipayan0320@gmail.com>
2023-12-18 23:22:54 +0530

    Project Setup: Custom Package Installed

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="header">
+          DRepo
+        </div>
+
+        <div className="grid">
+        </div>
+      </div>
+    </main>
+  );
+}
